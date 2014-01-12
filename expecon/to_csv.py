138005033f09855270cec936d0a82c2fc07753e5
RedwoodAdmin <jeremyhewett@alumni.ubc.ca>
2014-01-12 01:05:25 -0800

    Reformatted csv output data.
    Changed output data csv to use json for complex values.

--- expecon/to_csv.py
+++ expecon/to_csv.py
@@ -39,22 +39,10 @@ def parse_config(o):
 		configs[period][group] = config
 	return configs
 
+
 def queue_to_csv(l):
 	rows = []
 	header =  ['Period', 'Group', 'Sender', 'Time', 'ClientTime', 'Key', 'Value']
-	for o in l:
-		for col in build_header(o):
-			if col not in header and col not in ['Nonce', 'Session', 'Instance', 'StateUpdate']:
-				header.append(col)
-	for o in l:
-		if o['Key'] == '__set_config__':
-			configs = parse_config(o['Value'])
-			for period in configs.keys():
-				for group in configs[period].keys():
-					for col in build_header(configs[period][group]):
-						col = 'Config.' + col
-						if col not in header and col not in ['Config.period', 'Config.group']:
-							header.append(col)
 	rows.append(header)
 	groups = {}
 	configs = {}
@@ -71,34 +59,24 @@ def queue_to_csv(l):
 		else:
 			group = 0
 		for col in header:
-			if col.startswith('Config'):
-				col = col[7:]
-				if period not in configs:
-					continue
-				if group in configs[period]:
-					v = configs[period][group]
+			v = o
+			if type(v) == dict and col in v:
+				v = v[col]
+			elif type(v) == dict and col not in v:
+				v = ''
+			elif type(v) == list:
+				try:
+					index = int(col)
+				except ValueError:
+					index = -1
+				if index >= 0 and index < len(v):
+					v = v[index]
 				else:
-					v = configs[period][0]
+					v = ''
 			else:
-				v = o
-			for key in col.split('.'):
-				if type(v) == dict and key in v:
-					v = v[key]
-				elif type(v) == dict and key not in v:
-					v = ''
-				elif type(v) == list:
-					try:
-						index = int(key)
-					except ValueError:
-						index = -1
-					if index >= 0 and index < len(v):
-						v = v[index]
-					else:
-						v = ''
-				else:
-					v = ''
+				v = ''
 			if type(v) == dict or type(v) == list:
-				row.append('')
+				row.append(json.dumps(v))
 			else:
 				row.append(v)
 		rows.append(row)
